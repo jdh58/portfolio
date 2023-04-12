f4fd832c4ecc76581a34aa7f21950b9ef944ec14
Jonathan Hawes <jhawes580@gmail.com>
2023-04-12 15:17:29 -0700

    Add technology icons and better styling

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,24 @@
 import TechIcon from './TechIcon';
+import CPP from '../assets/TechLogos/cpp.svg';
+import CSS from '../assets/TechLogos/css.svg';
+import Express from '../assets/TechLogos/express.png';
+import Figma from '../assets/TechLogos/figma.svg';
+import Firebase from '../assets/TechLogos/firebase.svg';
+import Git from '../assets/TechLogos/Git.svg';
+import GitHub from '../assets/TechLogos/github.svg';
+import HTML from '../assets/TechLogos/html.svg';
 import JS from '../assets/TechLogos/javascript.svg';
+import Jest from '../assets/TechLogos/jest.png';
+import MongoDB from '../assets/TechLogos/mongodb.svg';
+import MySQL from '../assets/TechLogos/mysql.png';
+import Node from '../assets/TechLogos/nodejs.svg';
+import NPM from '../assets/TechLogos/npm.svg';
+import Photoshop from '../assets/TechLogos/photoshop.png';
+import React from '../assets/TechLogos/react.svg';
+import Redux from '../assets/TechLogos/redux.svg';
+import Tailwind from '../assets/TechLogos/tailwind.svg';
+import TS from '../assets/TechLogos/typescript.svg';
+import Webpack from '../assets/TechLogos/webpack.png';
 import '../styles/AboutPage.css';
 
 export default function AboutPage() {
@@ -23,18 +42,32 @@ export default function AboutPage() {
       <div className="techContainer">
         <h2 className="techHeader">Technologies</h2>
         <div className="techGrid">
+          <TechIcon imgURL={HTML} name="HTML" />
+          <TechIcon imgURL={CSS} name="CSS" />
+          <TechIcon imgURL={Tailwind} name="Tailwind" />
           <TechIcon imgURL={JS} name="Javascript" />
-          <TechIcon />
-          <TechIcon />
-          <TechIcon />
-          <TechIcon />
+          <TechIcon imgURL={TS} name="Typescript" />
+          <TechIcon imgURL={React} name="React" />
+          <TechIcon imgURL={Redux} name="Redux" />
+          <TechIcon imgURL={Node} name="Node.js" />
+          <TechIcon imgURL={Express} name="Express" />
+          <TechIcon imgURL={MongoDB} name="MongoDB" />
+          <TechIcon imgURL={Jest} name="Jest" />
+          <TechIcon imgURL={CPP} name="C++" />
+          <TechIcon imgURL={MySQL} name="MySQL" />
+          <TechIcon imgURL={Firebase} name="Firebase" />
+          <TechIcon imgURL={Webpack} name="Webpack" />
+          <TechIcon imgURL={Git} name="Git" />
+          <TechIcon imgURL={GitHub} name="GitHub" />
+          <TechIcon imgURL={NPM} name="NPM" />
         </div>
       </div>
       <div className="softwareContainer">
         <h2 className="softwareHeader">Software</h2>
-        <TechIcon />
-        <TechIcon />
-        <TechIcon />
+        <div className="softwareGrid">
+          <TechIcon imgURL={Photoshop} name="Photoshop" />
+          <TechIcon imgURL={Figma} name="Figma" />\{' '}
+        </div>
       </div>
     </div>
   );
