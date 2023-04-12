2a989a8415c8d841705b843a6bac20212ddc3594
Jonathan Hawes <jhawes580@gmail.com>
2023-04-12 16:33:20 -0700

    Add current tab indicator to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import '../styles/Header.css';
 
 export default function Header() {
+  const [currentTab, setCurrentTab] = useState(null);
+
+  //TODO: have the onclick just scroll, and set the currenttab based off scroll position
+
   return (
     <header className="header">
-      <a href="#aboutPage" className="About">
+      <a
+        href="#aboutPage"
+        className={currentTab === 'about' ? 'About active' : 'about'}
+        onClick={() => {
+          setCurrentTab('about');
+        }}
+      >
         <p>About</p>
       </a>
-      <a href="#projectsPage" className="Projects">
+      <a
+        href="#projectsPage"
+        className={currentTab === 'projects' ? 'projects active' : 'projects'}
+        onClick={() => {
+          setCurrentTab('projects');
+        }}
+      >
         <p>Projects</p>
       </a>
-      <a href="#contactPage" className="Contact">
+      <a
+        href="#contactPage"
+        className={currentTab === 'contact' ? 'contact active' : 'contact'}
+        onClick={() => {
+          setCurrentTab('contact');
+        }}
+      >
         <p>Contact</p>
       </a>
     </header>
