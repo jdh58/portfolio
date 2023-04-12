9b7d1b4eb4735906f94ac0ba2a33ee5c76043818
Jonathan Hawes <jhawes580@gmail.com>
2023-04-12 16:21:49 -0700

    Finish About Page styling for now.

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -34,12 +34,11 @@ export default function AboutPage() {
         </div>
         <div className="separator"></div>
         <div className="aboutText">
-          I'm a software developer in Portland, Oregon. Currently, I hold an
-          Associate's of Science in Computer Science, and am pursuing the
-          Bachelor's at Portland State University. Outside of my education, I
-          have been learning Web Development technologies and building projects.
-          This site serves as my personal portfolio, and hosts my projects.
-          Please enjoy!
+          I'm a software developer in Portland, Oregon. I hold an Associate of
+          Science in Computer Science, and am currently pursuing the Bachelor's
+          at Portland State University. Outside of my education, I have been
+          learning Web Development technologies and building projects. This site
+          serves as my personal portfolio, and hosts my projects. Please enjoy!
         </div>
       </div>
       <div className="techContainer">
