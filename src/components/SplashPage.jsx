23042118a8deed6278b115491f9096b5b6ef5634
Jonathan Hawes <jhawes580@gmail.com>
2023-04-12 01:08:53 -0700

    Mostly style Splash page.

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,8 +1,12 @@
+import '../styles/SplashPage.css';
+
 export default function SplashPage() {
   return (
     <div className="splashPage page">
       <div className="splashText">
-        <h1 className="hello">Hi, I'm Jonathan</h1>
+        <h1 className="hello">
+          Hi, I'm <p className="name">Jonathan.</p>
+        </h1>
         <h2 className="welcome">Welcome to my website</h2>
       </div>
     </div>
