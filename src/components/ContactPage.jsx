58fdc2baaed84cae829cf788930cf55f3562f277
Jonathan Hawes <jhawes580@gmail.com>
2023-04-13 12:25:29 -0700

    Style the my links section

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,3 +1,8 @@
+import Send from '../assets/send.svg';
+import GitHub from '../assets/TechLogos/github.svg';
+import Twitter from '../assets/TechLogos/twitter.png';
+import Instagram from '../assets/TechLogos/instagram.png';
+import LinkedIn from '../assets/TechLogos/linkedin.svg';
 import '../styles/ContactPage.css';
 
 export default function ContactPage() {
@@ -15,8 +20,34 @@ export default function ContactPage() {
           id="message"
           placeholder="Your message..."
         ></textarea>
-        <button type="submit">Send Message</button>
+        <div className="buttonContainer">
+          <div className="background"></div>
+          <button type="submit">Send Message</button>
+          <img src={Send} alt="" />
+        </div>
       </form>
+      <div className="personalLinks">
+        <div className="linksHeaderContainer">
+          <h1>My Links</h1>
+          <div className="separator"></div>
+        </div>
+        <div className="linkContainer">
+          <img src={GitHub} alt="" className="linkIcon" />
+          <p className="linkName">github.com/jdh58</p>
+        </div>
+        <div className="linkContainer">
+          <img src={LinkedIn} alt="" className="linkIcon" />
+          <p className="linkName">linkedin.com/in/jonath-hawes-4</p>
+        </div>
+        <div className="linkContainer">
+          <img src={Twitter} alt="" className="linkIcon" />
+          <p className="linkName">twitter.com/jhawes58</p>
+        </div>
+        <div className="linkContainer">
+          <img src={Instagram} alt="" className="linkIcon" />
+          <p className="linkName">instagram.com/jhawes58</p>
+        </div>
+      </div>
     </div>
   );
 }
