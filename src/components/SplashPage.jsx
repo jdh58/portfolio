3780a929ae121fe5c36abbb30d0610e05d7cf695
Jonathan Hawes <jhawes580@gmail.com>
2023-04-13 23:19:30 -0700

    Finish adding most responsiveness. Version 1.0

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,7 +5,7 @@ export default function SplashPage() {
     <div className="splashPage page">
       <div className="splashText">
         <h1 className="hello">
-          Hi, I'm <p className="name">Jonathan.</p>
+          Hi, I'm<p className="name">Jonathan.</p>
         </h1>
         <h2 className="welcome">Welcome to my website</h2>
       </div>
