3af460115d47f6cb16f0a4d2dfbea648e3d891b1
Jonathan Hawes <jhawes580@gmail.com>
2023-07-29 14:27:52 -0700

    Add firebaseconfig to the git repo cuz it has to

---
+++ src/firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCCTIKqw5Ra3YuaHARUoPQvlJE8Lzg_hto',
+  authDomain: 'portfolio-d10f7.firebaseapp.com',
+  projectId: 'portfolio-d10f7',
+  storageBucket: 'portfolio-d10f7.appspot.com',
+  messagingSenderId: '659984922874',
+  appId: '1:659984922874:web:2b9a6ea02abd648da7f143',
+  measurementId: 'G-D9RNXK5KPN',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
